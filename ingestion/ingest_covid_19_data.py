9d0c16301fbcec8315de8d1b4583e4a6a606887c
Samuel Favarin <samuel.favarin@paper.co>
2023-05-31 16:02:20 -0300

    feat: implements the full ingestion for covid data

--- ingestion/ingest_covid_19_data.py
+++ ingestion/ingest_covid_19_data.py
@@ -1,36 +1,93 @@
 from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
 from google.oauth2 import service_account
+from typing import List
 
-credentials = service_account.Credentials.from_service_account_file('../secret.json')
-client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
 
-def store_raw_data_on_bq():
-    pass
+class CovidIngestion:
 
-def get_raw_data():
+    def __init__(self):
+        self.credentials: service_account.Credentials = service_account.Credentials.from_service_account_file('../secret.json')
+        self.client: bigquery.Client = bigquery.Client(credentials=self.credentials, project=self.credentials.project_id)
+        self.dataset_id: str = f"{self.credentials.project_id}.raw"
 
-    query = """
-        SELECT
-            country_name,
-            country_code,
-            date,
-            SUM(new_confirmed) AS total_new_confirmed,
-            SUM(new_deceased) AS total_new_deceased,
-            SUM(new_persons_vaccinated) AS total_new_persons_vaccinated
-        FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
-        WHERE
-            country_name IN ("Spain", "Greece", "Turkey", "Italy", "Egypt", "Portugal") AND
-            date BETWEEN '2020-01-01' AND '2020-12-31'
-        GROUP BY
-            country_name,
-            country_code,
-            date
-        ORDER BY date;
-    """
+    @staticmethod
+    def _get_schema() -> List[bigquery.SchemaField]:
+        return  [
+            bigquery.SchemaField("country_name", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("country_code", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("date", "DATE", mode="REQUIRED"),
+            bigquery.SchemaField("total_new_confirmed", "INTEGER"),
+            bigquery.SchemaField("total_new_deceased", "INTEGER"),
+            bigquery.SchemaField("total_new_persons_vaccinated", "INTEGER"),
+        ]
 
-    return client.query(query)
+    @staticmethod
+    def _get_ingestion_query() -> str:
+        return """
+            SELECT
+                country_name,
+                country_code,
+                date,
+                SUM(new_confirmed) AS total_new_confirmed,
+                SUM(new_deceased) AS total_new_deceased,
+                SUM(new_persons_vaccinated) AS total_new_persons_vaccinated
+            FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
+            WHERE
+                country_name IN ("Spain", "Greece", "Turkey", "Italy", "Egypt", "Portugal") AND
+                date BETWEEN '2020-01-01' AND '2020-12-31'
+            GROUP BY
+                country_name,
+                country_code,
+                date
+            ORDER BY date;
+        """
+
+    def _create_dataset_if_not_exists(self, table_id: str):
+
+        try:
+            # check if table already exists
+            self.client.get_dataset(self.dataset_id)
+            print("Dataset {} already exists.".format(self.dataset_id))
+
+        except NotFound:
+            # create dataset
+            dataset = bigquery.Dataset(f"{self.credentials.project_id}.{self.dataset_id}")
+            dataset.location = "US"
+            dataset = self.client.create_dataset(dataset, timeout=30)
+            print("Created dataset {}.{}".format(self.client.project, dataset.dataset_id))
+
+    def _create_table_if_not_exists(self, table_id: str):
+
+        try:
+            # check if table already exists
+            self.client.get_table(table_id)
+            print("Table {} already exists.".format(table_id))
+
+        except NotFound:
+            # create table
+            table = bigquery.Table(table_id, schema=self._get_schema())
+            table = self.client.create_table(table)
+            print("Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id))
+
+    def _store_results(self, table_id: str):
+
+        # add results to table
+        job_config = bigquery.QueryJobConfig(destination=table_id, write_disposition="WRITE_TRUNCATE")
+        query_job = self.client.query(self._get_ingestion_query(), job_config=job_config)
+        query_job.result()
+        print("Query results loaded to the table {}".format(table_id))
+
+    def run_ingestion(self):
+
+        table_id = f"{self.dataset_id}.covid_2020"
+
+        self._create_dataset_if_not_exists(table_id)
+        self._create_table_if_not_exists(table_id)
+        self._store_results(table_id)
+
 
 if __name__ == '__main__':
 
-    for row in get_raw_data():
-        print(row['country_name'], row['date'], row['total_new_confirmed'])
+    covid_ingestion = CovidIngestion()
+    covid_ingestion.run_ingestion()
